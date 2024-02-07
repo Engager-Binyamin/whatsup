756a93c7f79819278020170669138f782b1de8c9
orna <o0rna@gmail.com>
2024-02-07 16:53:31 +0200

    workin whatsapp microservice

--- index.js
+++ index.js
@@ -1,48 +1,93 @@
 const express = require('express');
-const { Client, LocalAuth, MessageMedia, Whatsapp } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-const { ChatTypes, MessageAck } = require('whatsapp-web.js/src/util/Constants');
-const puppeteer = require('puppeteer');
-const app = express()
+const fs = require('fs').promises
+const { Server } = require('socket.io');
+const cors = require('cors');
 const port = 3636
-const { createServer } = require('node:http');
-const server = createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+
+const app = express();
+const server = require('http').Server(app);
+// const io = new Server(server);
+
+// app.use(cors());
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST'],
+        credentials: true,
+    },
+});
+
+
+
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+}));
 
 const client = new Client({
-    puppeteer: {
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    },
     authStrategy: new LocalAuth(),
 });
 
-io.on('connection', (socket) => {
-    socket.emit("qr", currentQRCode);
 
-    socket.on("howdy", (data) => {
-        console.log(data)
-    })
-})
+let currentQRCode = "";
 
-let currentQRCode = "";  // יש להוסיף משתנה שיחזיק את ה-QR code הנוכחי
 
-client.on('qr', (qr) => {
-    qrcode.generate(qr, { small: true });
-    console.log('QR RECEIVED', qr);
-    currentQRCode = qr;  // שמירת ה-QR code הנוכחי
-    // אפשר כמובן להפעיל פונקציה אחרת כאן שמתייחסת ל-QR code
+const { exec } = require('child_process');
+
+
+const deleteFolderRecursive = async (folderPath) => {
+    try {
+        const files = await fs.readdir(folderPath);
+
+        for (const file of files) {
+            const filePath = `${folderPath}/${file}`;
+            const fileStats = await fs.stat(filePath);
+
+            if (fileStats.isDirectory()) {
+                // אם הפריט הוא תיקייה, קרא לעצמך רקורסיבית כדי למחוק את התיקייה
+                await deleteFolderRecursive(filePath);
+            } else {
+                // אם הפריט הוא קובץ, סגור אותו ואז מחק
+                const fileHandle = await fs.open(filePath, 'r+');
+                await fileHandle.close();
+                await fs.unlink(filePath);
+            }
+        }
+
+        // לאחר שכל הפריטים נמחקו, מחק את התיקייה עצמה
+        await fs.rmdir(folderPath);
+    } catch (error) {
+        console.error('Error deleting folder and files:', error);
+    }
+};
+
+client.on('disconnected', async () => {
+
+    const folder = "./.wwebjs_auth"
+
+  await deleteFolderRecursive(folder).then(
+  client.initialize())
+
 });
-
-async function qrSend() {
-    const currentQRCode = qr;  // החזרת ה-QR code הנוכחי
-    io.emit('qr', currentQRCode)
-}
 
 client.on('ready', () => {
     console.log('Client is ready!');
+    io.emit('ready'); // שלח הודעה לצד הלקוח כאשר הלקוח מוכן
 });
+
+const getQr = () => {
+    client.on('qr', (qr) => {
+        qrcode.generate(qr, { small: true });
+        io.emit('qr', qr); // שלח את ה-QR code גם לצד הלקוח
+    });
+}
+
+getQr()
 
 client.on('message', msg => {
     if (msg.body == 'ping') {
@@ -62,13 +107,9 @@ isSending = false;
 
 async function sendNewMessage(data) {
     const rtrnData = {
-        phone: data.phone,
-        name: data.name,
-        _idL: data._id,
-        msg: data.msg,
+        _idL: data._idL,
         _idM: data._idM,
         idC: data.idC,
-        withName: data.withName,
         issend: "Message sent successfully"
     };
     const newData = data;
@@ -76,29 +117,31 @@ async function sendNewMessage(data) {
 
     const isWhatsApp = await client.isRegisteredUser(chatId);
     if (!isWhatsApp) {
-        console.log("Not a WhatsApp user");
         rtrnData.issend = "Not a WhatsApp user";
         io.emit("send", { rtrnData });
         return;
     }
 
     try {
-        delay(6000 + Math.random() * 1000)
         if (newData.withName) {
             client.sendMessage(chatId, `שלום ${newData.name}, ${newData.msg}`)
                 .then(() => {
                     io.emit("send", { rtrnData })
+                }).then(()=>{
+                    delay(6000 + Math.random() * 1000)
                 })
         }
         else {
             client.sendMessage(chatId, newData.msg)
                 .then(() => {
                     io.emit("send", { rtrnData });
+                }).then(()=>{
+                    delay(6000 + Math.random() * 1000)
                 })
                 .catch(error => {
                     console.error("Error sending message:", error);
                     io.emit("send", { message: "Error sending message" });
-                });
+                })
         }
     } catch (error) {
         console.error("Error sending message:", error);
@@ -106,7 +149,9 @@ async function sendNewMessage(data) {
     }
 }
 
-
+async function delay(t) {
+    return new Promise(resolve => setTimeout(resolve, t))
+}
 io.on('connection', async (socket1) => {
     socket1.on('data', (data) => {
         console.log(data);
@@ -114,28 +159,13 @@ io.on('connection', async (socket1) => {
             sendNewMessage(data);
         } catch (error) {
             console.error("Error sending message:", error.message);
-            // אפשר לשלוח הודעת שגיאה חזרה ללקוח דרך הסוקט, לדוג':
-            // socket1.emit('error', { message: 'Failed to send message' });
+        
         }
     });
 });
 
 
-async function delay(t) {
-    return new Promise(resolve => setTimeout(resolve, t))
-}
 
-
-app.use('/qrcode', express.static('qrcode'))
-
-app.get('/', async (req, res) => {
-    try {
-        qrSend()
-        res.send('Hello World!')
-    } catch (error) {
-        res.send(error)
-    }
-})
 
 server.listen(3000, () => {
     console.log('Server is running on port 3000');
