640152fea8527e982beb3b5967f8f797d56b41b5
kobikru <75983501+kobikru@users.noreply.github.com>
2024-03-13 13:43:30 +0200

    for the beginning

--- index.js
+++ index.js
@@ -3,16 +3,33 @@ const express = require("express");
 const mainRouter = require("./routes");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || "3000";
+const SOCKET_URL = process.env.SOCKET_URL || "http://localhost:5000";
 const db = require("./DL/db");
 const { getUsers } = require("./BL/account.service");
-const { io } = require("socket.io-client");
-const socket1 = io("http://localhost:" + PORT);
+const { Server } = require("socket.io");
+const http = require("http");
+// const socket1 = io(SOCKET_URL + PORT);
+const { startClient } = require("./DL/controllers/clientInitialize");
+const server = http.createServer(app);
 
 db.connect();
-app.use("/whatsUpServer", mainRouter);
+app.use("/message", mainRouter);
 app.use(express.json());
 app.use(cors());
+
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173"],
+  },
+});
+
+io.on("qr", (socket) => {
+  console.log(`user connected: ${socket.id}`);
+  socket.on("qr", (data) => {
+    console.log("hello");
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,50 @@
 const express = require("express");
 const mainRouter = express.Router();
-const messagesRouter = require("./messages.route");
-const userRouter = require("./user.route");
 
-mainRouter.use("/user", userRouter);
-mainRouter.use("/messages", messagesRouter);
+// message/send/
+// שליחת הודעה יזומה
+//  מקבל יוזר כטוקן
+//  מקבל מזהה קמפיין ומזהה הודעה
+// שולף לידים ופרטים נחוצים לפי הקמפיין + פרטי הודעה לפי מזהה הודעה
+// מעביר לפונ' הזרקת משתנים דינאמיים
+// שומר כל הודעה בטבלת "תור עבודה" לפי אלגוריתם של דיליי
+
+mainRouter.post("/send/", async (req, res) => {
+  try {
+    res.send(qr);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+//   message/send/jobs/
+//   שליחת הודעה בתזמון מאוחר
+// כשנשמר התזמון של הודעה )הוספת הודעה חדשה או עדכון( - קורה בשרת המרכזי
+// ויש בקשה מהמרכזי לסרוויס הנ"ל עם כל הפרטים.
+// מבצע את אותן פעולות כמו בשליחת הודעה יזומה
+//  צריך להוסיף לטבלת "Job של השרת" )תזמוני השרת( פעולה להפעיל תור עבודה
+// בתאריך-שעה שרצו לתזמן את ההודעות.
+
+mainRouter.post("/send/jobs/", async (req, res) => {
+  try {
+    res.send(qr);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+//   message/lead/:leadId/
+//   הודעת התנעה - הודעת 0
+//    כאשר יש ליד חדש שנכנס לקמפיין )מ-webhook , ידני או מקובץ( - קורה בשרת
+//   המרכזי ויש בקשה מהמרכזי לסרוויס הנ"ל עם כל הפרטים.
+//    נכנס לתור עבודה כמו שליחת הודעה יזומה.
+
+mainRouter.post("/send/lead/:leadId/", async (req, res) => {
+  try {
+    res.send(qr);
+  } catch (err) {
+    res.send(err);
+  }
+});
 
 module.exports = mainRouter;
