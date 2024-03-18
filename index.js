edb17c3ea8d60a9d82e45d2eb123894151737b35
kobikru <75983501+kobikru@users.noreply.github.com>
2024-03-18 12:08:57 +0200

    after matanel and miryam

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ db.connect();
 app.use(express.json());
 const http = require("http");
 const { createServer, clients } = require("./socket"); // כאן מייבאים את createServer ואת המשתנה clients מקובץ ה socket
-const router = require("./sendMessage.router"); // נייבא את הראוטר המוגדר בקובץ router.js
+const mainRouter = require("./routes");
 
 const server = http.createServer(app);
-app.use("/messages", router);
+
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -18,6 +18,5 @@ app.use(
     credentials: true,
   })
 );
-app.use("/sendmessage", router); // ראוט עבור שליחת הודעות
-// let fakeCampaign = {campaignId:"65eda5d5a53246c4f887ce33","65eda5d5a53246c4f887ce34"}
+app.use("/", mainRouter);
 createServer(server);
